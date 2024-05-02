db940bf93075a5abc64524ef67b1d456bbc50aec
Hassan Imran <43490649+hassan-imran@users.noreply.github.com>
2024-05-02 20:52:36 +0500

    Recreating frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import TodoForm from './TodoForm';
-import React, { useState } from 'react';
-import { Card, Typography } from '@mui/joy';
-import TodoList from './TodoList';
-import NavBar from './NavBar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
-  const auth = useSelector((state) => state.auth.value);
-  const dispatch = useDispatch();
-  // const [tasks, setTask] = useState([
-  //   {
-  //     id: 1,
-  //     text: 'Dummy Task',
-  //     completed: false,
-  //   },
-  // ]);
-
   return (
-    <div>
-      <NavBar />
-      <Card className="App" style={{
-        margin: 'auto',
-        marginTop: '5em',
-        maxWidth: '50%',
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-      }}>
-        <Typography level='h1' style={{ marginTop: '0.25em' }}>Todo List</Typography>
-        <TodoForm />
-        <TodoList />
-      </Card>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
